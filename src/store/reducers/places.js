c0502004cbbffcc162e874c2a9c513a77b066fd6
marwolaethblack <a.unal677@gmail.com>
2018-03-20 12:04:02 +0100

    add reducer cases

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,14 +1,60 @@
+import * as AT from '../actions/actionTypes';
+import placeImage from '../../assets/niceplace.jpg';
+
 const initialState = {
     places: [],
     selectedPlace: null
-  };
+};
 
-const placesReducer = (state=initialState, action) => {
-    switch(action.type) {
+const placesReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case AT.ADD_PLACE: {
+
+            if (action.placeName.trim() === "") {
+                return state;
+            }
+
+            return {
+                ...state,
+                places: state.places.concat({
+                    placeName: action.placeName,
+                    key: Math.random(),
+                    image: placeImage
+                })
+            }
+        }
+
+        case AT.DELETE_PLACE: {
+            return {
+                ...state,
+                places: state.places.filter((p) => {
+                    return p.key !== action.key
+                }),
+                selectedPlace: null
+            }
+        }
+
+        case AT.SELECT_PLACE: {
+            return {
+                ...state,
+                selectedPlace: state.places.find(p => {
+                    return p.key === action.key;
+                })
+            }
+        }
+
+        case AT.DELETE_PLACE: {
+            return {
+                ...state,
+                selectedPlace: null
+            }
+        }
+
         default:
-        return state;
+            return state;
     }
 
-} 
+}
 
 export default placesReducer
